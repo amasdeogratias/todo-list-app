2b3f987e361b9d65a0a8bbdef0fecd5fe876c663
DeogratiasAmas <deogratiasprog@gmail.com>
2026-03-30 16:28:35 +0300

    added add todo form component

--- src/App.tsx
+++ src/App.tsx
@@ -1,10 +1,12 @@
 import { useState } from 'react'
+import TodoForm from './components/TodoForm'
 
 function App() {
 
   return (
-    <div>
-      <h1>Todo App</h1>
+    <div className='container mx-auto p-4'>
+      <h1 className='text-2xl font-bold mb-4'>Todo App</h1>
+      <TodoForm  />
     </div>
   )
 }
---
+++ src/components/TodoForm.tsx
@@ -0,0 +1,19 @@
+import React, { useState } from 'react'
+
+const TodoForm = () => {
+    const [newTodo, setNewTodo] = useState<string>("")
+  return (
+    <form className='flex gap-2'>
+        <input 
+            type="text" 
+            name="todo" 
+            id="todo"
+            className='border border-gray-300 rounded px-2 py-1 flex-1'
+            onChange={(e) => setNewTodo(e.target.value)} 
+        />
+        <button type="submit" className='bg-green-500 text-white rounded px-4 py-2 cursor-pointer'>Add Todo</button>
+    </form>
+  )
+}
+
+export default TodoForm
